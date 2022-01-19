72959170223df39a333b1b7dabcffd149ec9b960
pedroeproess <estudo.pedro.repositorio@outlook.com>
2022-01-18 21:39:32 -0300

    Seller created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,17 @@
 package main;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(obj);
+        System.out.println(seller);
 
 
 
