6821ce83f48575982c248d01f38bb6a8628dc5a9
pedroeproess <estudo.pedro.repositorio@outlook.com>
2022-01-18 21:29:04 -0300

    Department created

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        Department obj = new Department(1, "books");
+        System.out.println(obj);
 
 
 
